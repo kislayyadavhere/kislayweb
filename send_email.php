06c6820b2fea52d63424638e4316036bc5199d53
kislayyadavhere <165045929+kislayyadavhere@users.noreply.github.com>
2024-03-26 19:58:48 +0530

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['send_message_btn'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  // Set the email headers.
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= "From: $name <$email>" . "\r\n";
+
+  // Replace 'your_email@example.com' with the actual recipient email address
+  $to = 'your_email@example.com';  
+
+  // Send the email.
+  $sent = mail($to, $subject, $message, $headers);
+
+  if ($sent) {
+    // Redirect the user to a success page.
+    header('Location: success.php');
+  } else {
+    // Display an error message if email fails to send
+    echo 'Error sending email!';
+  }
+}
+
+?>
